8fbafb6a3eb29872ff5edad3755024dfb6d0f30b
CarolineBenn <lineybenn@gmail.com>
2017-09-29 00:37:42 +0100

    Implement Proxima Nova Soft typeface

--- src/components/NeighbourhoodHero/index.jsx
+++ src/components/NeighbourhoodHero/index.jsx
@@ -6,7 +6,7 @@ const NeighbourhoodHero = ({ count, description, hero, name }) => (
   <div className="neighbourhood__hero-container" style={{ backgroundImage: `url(${hero})` }}>
     <div className="neighbourhood__hero-details">
       <span>{count} restaurants delivering to</span>
-      <h1>{name}</h1>
+      <h1 className="neighbourhood_hero-name">{name}</h1>
       <p>{description}</p>
     </div>
     <div className="overlay" />
