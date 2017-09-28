2d41b32dc6b5adceb0a41eaf7810bd86d51160bc
CarolineBenn <lineybenn@gmail.com>
2017-09-28 21:50:51 +0100

    Add data

---
+++ src/data/data.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+
+export const restaurants = [
+  {
+    id: 1,
+    name: 'Bobo Social',
+    tags: ['Vietnamese'],
+    priceCategory: '££',
+    time: '30',
+    url: '/bobo-social'
+  },
+  {
+    id: 2,
+    name: 'Tsunami',
+    tags: ['Vietnamese'],
+    priceCategory: '££',
+    time: '30',
+    url: 'tsunami'
+  },
+  {
+    id: 3,
+    name: 'Wild Game Co',
+    tags: ['Vietnamese'],
+    priceCategory: '££',
+    time: '30',
+    url: 'wild-game-co'
+  }
+];
+
+export const restaurant = {
+  name: 'Bobo Social',
+  tags: ['Burgers'],
+  priceCategory: '££',
+  blurb: 'Serving the highest-quality burgers and sides with an array of creative and tasty toppings, Bobo Social cooks its rare-breed beed in a special oven to take you to burger heaven!',
+  menu: [
+    {
+      category: 'Burgers',
+      options: [
+        {
+          id: 1,
+          name: 'Bobo',
+          desc: '6oz rare-breed beef, lettuce, tomatoes, charcoal-roasted red onions, BOBO sauce',
+          price: '£8.95',
+        },
+        {
+          id: 2,
+          name: 'Bobo with Cheese',
+          desc: '6oz rare-breed beef, Westcombe Cheddar, lettuce, tomatoes, charcoal-roasted red onions, BOBO sauce',
+          price: '£9.95',
+        }
+      ],  
+    }
+  ],
+};
+
+
+const neighbourhood = {
+  name: 'Hampstead',
+  description: 'Hampstead is a great place to while away a whole day. As well as three of London’s biggest and best museums, there’re plenty of other things to do that are sure to entertain and amuse everyone in your group.',
+  count: 34,
+  restaurants: restaurants,
+};
