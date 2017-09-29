37839b7c8532bd1a1f41b083e6bfaaf91346f0aa
CarolineBenn <lineybenn@gmail.com>
2017-09-29 16:23:08 +0100

    Remove unused data

--- src/data/index.js
+++ src/data/index.js
@@ -25,33 +25,6 @@ export const restaurants = [
   }
 ];
 
-export const restaurant = {
-  name: 'Bobo Social',
-  tags: ['Burgers'],
-  priceCategory: '££',
-  blurb: 'Serving the highest-quality burgers and sides with an array of creative and tasty toppings, Bobo Social cooks its rare-breed beed in a special oven to take you to burger heaven!',
-  menu: [
-    {
-      category: 'Burgers',
-      options: [
-        {
-          id: 1,
-          name: 'Bobo',
-          desc: '6oz rare-breed beef, lettuce, tomatoes, charcoal-roasted red onions, BOBO sauce',
-          price: '£8.95',
-        },
-        {
-          id: 2,
-          name: 'Bobo with Cheese',
-          desc: '6oz rare-breed beef, Westcombe Cheddar, lettuce, tomatoes, charcoal-roasted red onions, BOBO sauce',
-          price: '£9.95',
-        }
-      ],  
-    }
-  ],
-};
-
-
 export const neighbourhood = {
   name: 'Hampstead',
   description: 'Hampstead is a great place to while away a whole day. As well as three of London’s biggest and best museums, there’re plenty of other things to do that are sure to entertain and amuse everyone in your group.',
