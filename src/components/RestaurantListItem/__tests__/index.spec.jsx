d2a5b59358fa4ade944edcf24d907b681a755934
CarolineBenn <lineybenn@gmail.com>
2017-09-29 14:02:06 +0100

    Upadate RestuarantListItem snapshot

--- src/components/RestaurantListItem/__tests__/index.spec.jsx
+++ src/components/RestaurantListItem/__tests__/index.spec.jsx
@@ -3,12 +3,14 @@ import renderer from 'react-test-renderer';
 
 import RestaurantListItem from '..';
 
+import { restaurants } from '../../../data';
+
 // Mock components that have their own snapshot tests
 jest.mock('../../Arrow', () => 'Arrow');
 
 test('RestaurantListItem renders correctly', () => {
   const tree = renderer.create(
-    <RestaurantListItem />,
+    <RestaurantListItem {...restaurants[0]} />,
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
