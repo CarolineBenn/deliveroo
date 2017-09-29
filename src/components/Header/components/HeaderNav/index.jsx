9d56f9cc8ddc586d8f58f2c644f13f8fe95c4aba
CarolineBenn <lineybenn@gmail.com>
2017-09-29 14:48:00 +0100

    Create Header component

--- src/components/Header/components/HeaderNav/index.jsx
+++ src/components/Header/components/HeaderNav/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
-const HeaderNav = () => (
-  <nav>
-    <ul>
-      <li>Home</li>
-      <li>Food</li>
+import './styles.css';
+
+const HeaderNav = ({
+  mobile,
+}) => (
+  <nav className={`${mobile ? 'mobile-header-nav' : 'header-nav'}`}>
+    <ul className="unstyledList header-nav-menu">
+      <li><a href="/" className="header-nav-menu-item">Home</a></li>
+      <li><a href="#" className="header-nav-menu-item">Search</a></li>
+      <li><a href="#" className="header-nav-menu-item">£8.95</a></li>
+      <li><a href="#" className="header-nav-menu-item">Simon Rohrbach</a></li>
     </ul>
   </nav>
 );
 
 export default HeaderNav;
+
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -13,20 +13,26 @@ class Header extends Component {
   }
 
   toggleNav = () => {
-    // This function toggle the menu.
     this.setState({ showNav: !this.state.showNav })
   }
 
   render() {
+    const { showNav } = this.state;
     return (
       <header className="header">
-        <a className="site-name" href="/">Deliveroo</a>
-        <button className="reset menu-icon" onClick={this.toggleNav}>
-          <span className="srOnly">Toggle navigation</span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-        </button>
+        <div className="container header-inner-container">
+          <a className="site-name" href="/">Deliveroo</a>
+          <button className="reset menu-icon" onClick={this.toggleNav}>
+            <span className="srOnly">Toggle navigation</span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </button>
+          {showNav &&
+            // `mobile` prop indicates that 
+            <HeaderNav key="SM" mobile />}
+          <HeaderNav key="MDLG" />
+        </div>
       </header>
     );
   }
